b4b995f7d32337192198ddeb4d28ca152c91ec02
Nick Wang <wang703003515@gmail.com>
2022-09-06 14:18:29 -0500

    Assembles todos to view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
 import { Backdrop } from "./Components/Backdrop";
 import { Container } from "./Components/Container";
 import TitleRow from "./Components/TitleRow";
+import Todo from "./Components/Todo";
+import { todos } from "./Data/todos";
 
 function App() {
   return (
     <Backdrop>
       <Container>
         <TitleRow />
+        {todos.map(({ name, completed }) => {
+          return (
+            <Todo
+              key={name}
+              value={name}
+              completed={completed}
+              handleComplete={() => {}}
+              handleDelete={() => {}}
+            />
+          );
+        })}
       </Container>
     </Backdrop>
   );
---
+++ frontend/src/Data/todos.ts
@@ -0,0 +1,18 @@
+export const todos = [
+  {
+    name: "todo 1",
+    completed: false,
+  },
+  {
+    name: "todo 2",
+    completed: false,
+  },
+  {
+    name: "todo 3",
+    completed: false,
+  },
+  {
+    name: "todo 4",
+    completed: false,
+  },
+];
